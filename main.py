be79a866febfd8ca28bf95802a4e4072ad1e073b
Yamitzky <negiga@gmail.com>
2018-06-04 12:16:26 +0900

    Update main.py to fail fast if dict is broken

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 # -*- coding: utf-8 -*-
 import os
-from functools import lru_cache
 
 from bottle import route, request, default_app
 from natto import MeCab
 
 
-@lru_cache(maxsize=None)
-def tagger():
-    dicdir = os.environ.get('MECAB_DICDIR')
-    if dicdir:
-        return MeCab('-d {}'.format(dicdir))
-    else:
-        return MeCab()
+dicdir = os.environ.get('MECAB_DICDIR')
+if dicdir:
+    tagger = MeCab('-d {}'.format(dicdir))
+else:
+    tagger = MeCab()
 
 
 @route('/parse')
 def index():
     result = []
-    for line in tagger().parse(request.query.q).split('\n'):
+    for line in tagger.parse(request.query.q).split('\n'):
         line = line.strip()
         parts = line.split('\t', 1)
         if line == 'EOS' or len(parts) <= 1:
